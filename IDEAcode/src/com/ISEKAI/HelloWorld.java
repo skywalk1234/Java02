8e8b8c05c57f45b0a5e109c772639a97bb601bf0
mm <19868653158@163.com>
2024-10-06 21:59:57 +0800

    add Java02 to web

---
+++ IDEAcode/src/com/ISEKAI/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.ISEKAI;
+/*------------------------------------*/
+import com.ISEKAI.tool.Print;
+/*------------------------------------*/
+public class HelloWorld {
+    public static void main(String[] args){
+        Test.test();
+    }
+}
+/*------------------------------------*/
+class Test{
+    public static void test(){
+        Print.print("Hello World");
+    }
+}
---
+++ IDEAcode/src/com/ISEKAI/tool/Print.java
@@ -0,0 +1,7 @@
+package com.ISEKAI.tool;
+
+public class Print {
+    public static void print(String str){
+        System.out.println(str);
+    }
+}
